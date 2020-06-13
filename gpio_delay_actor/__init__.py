e8616d82c13bc799fe599368c75c8abda89f45ac
Tom Fenech <tomjwfenech@gmail.com>
2020-06-13 14:34:46 +0200

    Fix broken cast of GPIO pin number to int

--- gpio_delay_actor/__init__.py
+++ gpio_delay_actor/__init__.py
@@ -5,16 +5,20 @@ from modules import cbpi
 from modules.core.hardware import ActorBase
 from modules.core.props import Property
 
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+except Exception as e:
+    print e
+    pass
 
 
 @cbpi.actor
 class GPIODelay(ActorBase):
 
-    gpio = int(
-        Property.Select("GPIO", options=range(28),
-                        description="GPIO pin number")
-    )
+    gpio = Property.Select("GPIO", options=range(28),
+                           description="GPIO pin number")
     delay = Property.Number(
         "Minimum delay",
         configurable=True,
@@ -26,38 +30,40 @@ class GPIODelay(ActorBase):
     switched_off_at = None
 
     def init(self):
-        GPIO.setup(self.gpio, GPIO.OUT)
-        GPIO.output(self.gpio, 0)
+        gpio = int(self.gpio)
+        GPIO.setup(gpio, GPIO.OUT)
+        GPIO.output(gpio, 0)
 
     def on(self, power=0):
-        cbpi.app.logger.info("Request to switch on GPIO %d" % self.gpio)
-        if GPIO.input(self.gpio) == 1:
-            cbpi.app.logger.info("GPIO already on %d" % self.gpio)
+        gpio = int(self.gpio)
+        cbpi.app.logger.info("Request to switch on GPIO %d" % gpio)
+        if GPIO.input(gpio) == 1:
+            cbpi.app.logger.info("GPIO %d already on" % gpio)
             return
 
         if self.switched_off_at is not None:
             since_last_off = time.time() - self.switched_off_at
             cbpi.app.logger.info(
                 "GPIO %d last switched off %d seconds ago" %
-                (self.gpio, since_last_off)
+                (gpio, since_last_off)
             )
 
             if since_last_off < self.delay:
                 cbpi.app.logger.info(
-                    "Not enough time since last switched off GPIO %d" %
-                    self.gpio
+                    "Not enough time since last switched off GPIO %d" % gpio
                 )
                 return
 
-        cbpi.app.logger.info("Switching on GPIO %d" % self.gpio)
-        GPIO.output(self.gpio, 1)
+        cbpi.app.logger.info("Switching on GPIO %d" % gpio)
+        GPIO.output(gpio, 1)
 
     def off(self):
-        cbpi.app.logger.info("Request to switch off GPIO %d" % self.gpio)
-        if GPIO.input(self.gpio) == 0:
-            cbpi.app.logger.info("GPIO already off %d" % self.gpio)
+        gpio = int(self.gpio)
+        cbpi.app.logger.info("Request to switch off GPIO %d" % gpio)
+        if GPIO.input(gpio) == 0:
+            cbpi.app.logger.info("GPIO already off %d" % gpio)
             return
 
-        cbpi.app.logger.info("Switching off GPIO %d" % self.gpio)
+        cbpi.app.logger.info("Switching off GPIO %d" % gpio)
         self.switched_off_at = time.time()
-        GPIO.output(self.gpio, 0)
+        GPIO.output(gpio, 0)
